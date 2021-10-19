e76146bd4a4b364d1114098a3c074065ce2cff8c
NathanMinaev <natanm@gmail.com>
2021-10-19 16:51:14 +0300

    hw6 updated. issue with counter - doesn't update on first client

--- server.js
+++ server.js
@@ -1,44 +1,58 @@
-const { readdirSync, lstatSync, readFileSync } = require("fs");
 const http = require("http");
+const io = require("socket.io");
 const path = require("path");
+const fs = require("fs");
 
-// const isFile = (path) => lstatSync(path).isFile();
-const isDir = (path) => lstatSync(path).isDirectory();
+let counter = 0;
 
-let pathToCurrentDir = process.cwd();
-const baseUrl = "http://localhost:5555/";
-let filesInDirectory = null;
-let pathToSelected = null;
-let result = null;
-
-(async () => {
-  http
-    .createServer(async (req, res) => {
-      pathToSelected = path.join(pathToCurrentDir, req.url.slice(1));
-      try {
-        if (isDir(pathToSelected)) {
-          filesInDirectory = readdirSync(pathToSelected);
-          result = `<h1>File Manager</h1><h4><a href=${
-            baseUrl + req.url.split("/").slice(1, -1).join("/")
-          }>...</a></h4>`;
-          filesInDirectory.forEach(
-            (fileName) =>
-              (result += `<h4><a href=${
-                baseUrl + req.url.slice(1) + "/" + fileName
-              }>${fileName}</a></h4>`)
-          );
-          res.setHeader("Content-Type", "text/html");
-        } else {
-          result = readFileSync(pathToSelected, "utf-8");
-          res.setHeader("Content-Type", "text/plain");
-        }
-      } catch (e) {
-        result = e.toString();
-        res.setHeader("Content-Type", "text/plain");
-      }
-      res.end(result);
-    })
-    .listen(5555, () => {
-      console.log("the server started at " + baseUrl);
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    const filePath = path.join(__dirname, "index.html");
+    const rs = fs.createReadStream(filePath);
+    res.writeHead(200, {
+      "Content-Type": "text/html",
     });
-})();
+    rs.pipe(res);
+  }
+});
+
+const socket = io(server);
+
+socket.on("connection", (client) => {
+  console.log("new connection");
+  ++counter;
+
+  client.on("get-counter", () => {
+    console.log("counter event received", counter);
+    client.broadcast.emit("COUNTER", {
+      counter: counter,
+    });
+  });
+
+  client.broadcast.emit("NEW_CONN_EVENT", {
+    msg: "A new client connected",
+    nick: client.id,
+  });
+
+  client.on("CLIENT_MSG", (data) => {
+    client.emit("SERVER_MSG", {
+      msg: data.msg,
+      nick: client.id,
+    });
+    client.broadcast.emit("SERVER_MSG", {
+      msg: data.msg,
+      nick: client.id,
+    });
+  });
+
+  client.on("disconnect", () => {
+    counter = counter - 1;
+    client.broadcast.emit("NEW_DISCON_EVENT", {
+      msg: "Client disconnected",
+      nick: client.id,
+      counter: counter,
+    });
+  });
+});
+
+server.listen(5555);
